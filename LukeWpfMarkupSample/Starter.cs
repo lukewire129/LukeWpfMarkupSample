fcb8465de11348cd0813bc9109c401a6ebd8e0e7
lukewire129 <lukewire129@gmail.com>
2024-05-24 09:12:48 +0900

    초기 커밋.

---
+++ LukeWpfMarkupSample/Starter.cs
@@ -0,0 +1,11 @@
+﻿namespace LukeWpfMarkupSample;
+
+public class Starter
+{
+    [STAThread]
+    private  static void Main(string[] args)
+    {
+        _= new App ()
+               .Run ();
+    }
+}
---
+++ LukeWpfMarkupSample/globalusing.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using static CSharpMarkup.Wpf.Helpers;
+global using static System.Windows.Media.Colors;
+global using Base = System.Windows;
+global using ICommand = System.Windows.Input.ICommand;
+global using BindingMode = System.Windows.Data.BindingMode;
+global using Button_UI = System.Windows.Controls.Button;
+global using TextBlock_UI = System.Windows.Controls.TextBlock;
+global using TextBox_UI = System.Windows.Controls.TextBox;
+global using InlineCollection_UI = System.Windows.Documents.InlineCollection;
+global using ListViewItem_UI = System.Windows.Controls.ListViewItem;
+
+global using CSharpMarkup.WPF.Support;
+global using CSharpMarkup.Wpf;
